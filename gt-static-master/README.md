574470f7a7e71468f5691d3d4f274ce07d01b87f
dbanerjee87 <dbangatech30@gmail.com>
2017-04-23 19:40:22 -0400

    Add files via upload

---
+++ gt-static-master/README.md
@@ -0,0 +1,5 @@
+gt-static
+=========
+
+GT Theme with Static HTML
+Close match to version 2.6 of GT Theme for Drupal 7
---
+++ gt-static-master/index.php
@@ -0,0 +1,146 @@
+<?php include('inc/head.inc'); ?>
+<body class="front" >
+<div id="page">
+  <?php include('inc/masthead.inc'); ?>
+  <section role="main" id="main">
+    <div class="row clearfix">
+      <div id="page-title">
+        <!-- <h2 class="title">Home</h2> Normally the Front page title is hidden from view by moving off screen with CSS -->
+      </div>
+      <div class="no-sidebars" id="content">
+        <div class="region region-content block-count-1 clearfix">
+          <div class="block block-system">
+            <div class="content block-body clearfix">
+              <div class="node clearfix">
+                <div class="content">
+                   <div class="block-row block-row-odd block-count-2">
+                    <div class="row-content clearfix">
+                      <div class="block block-hg-reader promo-block block-region-weight-1 odd">
+                        <div class="block-title-wrapper">
+                          <h4>Important Dates</h4>
+                        </div>
+                        <div class="content block-body clearfix">
+                          <div class="hg-feed-wrapper hg-block">
+                            <ul class="hg-feed hg-feed-list">
+                              <li class="hg-feed-item clearfix">
+                                <div class="hg-feed-event"> <span class="hg-feed-event-month">Jan</span> <span class="hg-feed-event-day">1</span> </div>
+                                <h3 class="hg-feed-event-title"><a href="#">Lorem ipsum dolor sit amet</a></h3>
+                                <p class="hg-feed-event-summary-sentence">Onsectetur adipiscing elit. Curabitur aliquet lacinia tincidunt.</p>
+                              </li>
+                              <li class="hg-feed-item clearfix">
+                                <div class="hg-feed-event"> <span class="hg-feed-event-month">Jan</span> <span class="hg-feed-event-day">31</span> </div>
+                                <h3 class="hg-feed-event-title"><a href="#">Aenean elit tellus</a></h3>
+                                <p class="hg-feed-event-summary-sentence">Lorem tortor egestas tellus, non rutrum enim nisl at libero.</p>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="block block-hg-reader related-info-block icon-download-title">
+                        <div class="block-title-wrapper">
+                          <h4>News</h4>
+                        </div>
+                        <div class="content block-body clearfix">
+                          <div class="hg-feed-wrapper hg-block">
+                            <ul class="hg-feed hg-feed-list">
+                              <li class="hg-feed-item clearfix">
+                                <div class="hg-feed-news-thumbnail"> <a href="#"> <img src="/images/sampleimage-320x180.jpg" alt="Lorem ipsum dolor sit amet"> </a> </div>
+                                <h3 class="hg-feed-news-title"> <a href="#"> Lorem ipsum dolor sit amet</a> </h3>
+                                <div class="hg-feed-news-summary">
+                                  <p>Onsectetur adipiscing elit. Curabitur aliquet lacinia tincidunt. Nunc pellentesque eu leo in iaculis. Pellentesque id posuere urna. Etiam ex ante, congue ac eros id, hendrerit feugiat eros.</p>
+                                </div>
+                              </li>
+                              <li class="hg-feed-item clearfix">
+                                <div class="hg-feed-news-thumbnail"> <a href="#"> <img src="/images/sampleimage-320x180.jpg" alt="Aenean elit tellus"> </a> </div>
+                                <h3 class="hg-feed-news-title"> <a href="#">Aenean elit tellus</a> </h3>
+                                <div class="hg-feed-news-summary">
+                                  <p>Lorem tortor egestas tellus, non rutrum enim nisl at libero. Pellentesque pulvinar dolor et turpis convallis pellentesque. Nam vitae ligula eget tellus mattis tempus</p>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <br>
+                  <br>
+                  <div class="block-row block-count-3 clearfix">
+                    <div class="row-content clearfix">
+                      <div class="block third-width">
+                        <div class="content block-body clearfix">
+                          <div class="sb-skin-gold">
+                            <h4 class="super-block__title"> <a href="http://www.drupal.gatech.edu/node/320" class="super-block__title-link">Gold Block Styling</a></h4>
+                            <div class="content">
+                              <div class="super-block__image">
+                                <div class="super-block__image-wrapper"> <a href="http://www.drupal.gatech.edu/node/320"><img src="/images/sampleimage-320x180.jpg" width="320" height="180" alt="placeholder"/></a></div>
+                              </div>
+                              <div class="super-block__lower">
+                                <div class="super-block__teaser">
+                                  <p>Block with a title and &quot;<strong>Gold</strong>&quot; styling. </p>
+                                </div>
+                                <div class="super-block__jump-link"> <a href="http://www.drupal.gatech.edu/node/320">Learn more</a></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- / -->
+                      <div class="block third-width">
+                        <div class="content">
+                          <div class="sb-skin-default">
+                            <h4 class="super-block__title"> <a href="http://www.drupal.gatech.edu/node/320" class="super-block__title-link">Default Block Styling</a></h4>
+                            <div class="content">
+                              <div class="super-block__image">
+                                <div class="super-block__image-wrapper"> <a href="http://www.drupal.gatech.edu/node/320"><img src="/images/sampleimage-320x180.jpg" width="320" height="180" alt="placeholder"/></a></div>
+                              </div>
+                              <div class="super-block__lower">
+                                <div class="super-block__teaser">
+                                  <p>Block with a title and &quot;<strong>Default</strong>&quot; styling.</p>
+                                </div>
+                                <div class="super-block__jump-link"> <a href="http://www.drupal.gatech.edu/node/320">Learn more</a></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- / -->
+                      <div class="block third-width">
+                        <div class="content">
+                          <div class="sb-skin-blue">
+                            <h4 class="super-block__title"> <a href="http://www.drupal.gatech.edu/node/320" class="super-block__title-link">Blue Block Styling</a></h4>
+                            <div class="content">
+                              <div class="super-block__image">
+                                <div class="super-block__image-wrapper"> <a href="http://www.drupal.gatech.edu/node/320"><img src="/images/sampleimage-320x180.jpg"  width="320" height="180" alt="placeholder"/></a></div>
+                              </div>
+                              <div class="super-block__lower">
+                                <div class="super-block__teaser">
+                                  <p>Block with a title and &quot;<strong>Blue</strong>&quot; styling.</p>
+                                </div>
+                                <div class="super-block__jump-link clearfix"> <a href="http://www.drupal.gatech.edu/node/320">Learn more</a></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- / -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /#content -->
+    </div>
+  </section>
+  <!-- /#main -->
+
+  <?php /* include('inc/superfooter.inc');  */ ?>
+  <?php include('inc/footer.inc'); ?>
+</div>
+<!-- /#page -->
+
+</body>
+</html>
